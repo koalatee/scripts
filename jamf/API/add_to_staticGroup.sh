bacf4dd44f017f45d14496bb4c3df362cc77c5f2
James <james.journey@gmail.com>
2018-01-29 10:33:23 -0700

    Create add_to_staticGroup.sh

---
+++ jamf/API/add_to_staticGroup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##### Variables #####
+jamfURL="your.jamf.here:8443"
+serialNumber="$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')"
+groupID="" # static group ID here
+
+## Function for api account string decryption
+## https://github.com/jamfit/Encrypted-Script-Parameters
+function DecryptString() {
+    # Usage: ~$ DecryptString "Encrypted String" "Salt" "Passphrase"
+    echo "${1}" | /usr/bin/openssl enc -aes256 -d -a -A -S "${2}" -k "${3}"
+}
+## Decrypt username + password
+apiUser=$(DecryptString $4 'apiUser salt here' 'apiUser passphrase here')
+apiPass=$(DecryptString $5 'apiPass salt here' 'apiPass passphrase here')
+
+# Add to static group
+apiData="<computer_group><computer_additions><computer><serial_number>$serialNumber</serial_number></computer></computer_additions></computer_group>"
+# curl command
+curl \
+    -s \
+    -f \
+    -u ${apiUser}:${apiPass} \
+    -X PUT \
+    -H "Content-Type: text/xml" \
+    -d "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>$apiData" $jamfURL/JSSResource/computergroups/id/$groupID
+    
+exit 0
