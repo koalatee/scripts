5b57a5c87968bedd011216c4730c342b7bf3ceaa
James <james.journey@gmail.com>
2019-03-06 16:05:03 -0700

    Update CCLicenseType_EA.sh
    updated with new Acrobat check

--- jamf/EAs/CCLicenseType_EA.sh
+++ jamf/EAs/CCLicenseType_EA.sh
@@ -2,30 +2,61 @@
 
 ###### jjourney 01/2017 ######
 # Checks Adobe license type
-# Should output one of the following:
-# NONE
-# SUBSCRIPTION:$loggedInUser
-# VOLUME:$loggedInUser
+
+# Update 3/2019 
+# Checks Acrobat as well
+
+# Setup:
+# Enter appropriate variable changes
+# - 4 is encrypted user string
+# - 5 is encrypted pass string
+# - 6 is CC swidtag location (shouldn't change?)
+# - 7 is where the AdobeExpiryTool is located (see note below)
+# - 8 is trigger to download if not found (or you can include it with this policy)
+# - 9 is Creative Cloud License EA ID
+# - 10 is Acrobat Pro License EA ID
+# Setup a jamf policy with this script and the correct parameters
+# Setup another jamf policy with the trigger to install the AcrobatExpiryTool
+
+# API URL
+apiURL="https://your.jamf.here:8443"
+
+# Adobe SWID file
+# Should be:
+# /Library/Application Support/regid.1986-12.com.adobe/regid.1986-12.com.adobe_V7{}CreativeCloudEnt-1.0-Mac-GM-MUL.swidtag
+adobeCCFile="${6}"
+# Path to the Acrobat Tool https://helpx.adobe.com/enterprise/kb/volume-license-expiration-check.html
+acrobatTool="${7}"
+# some trigger to download the tool if not found
+DownloadAcrobatTool="${8}"
+# CC License EA ID
+CCLicenseEA="${9}"
+# Acrobat License EA ID
+AcrobatLicenseEA="${10}"
+
+## These go in your EA
+AcrobatNamedUserInstall="Acrobat Pro Named-User license installed"
+AcrobatSerialInstall="Acrobat Pro Serial license installed"
+AcrobatError="Unknown Issue with License Tool"
+AcrobatNotInstalled="Acrobat Pro Not Installed"
+CCNotInstalled="Creative Cloud not Installed"
+CCUninstalled="Creative Cloud uninstalled"
+CCError="Creative Cloud error"
 
 # Function to decrypt the string
+# https://github.com/jamf/Encrypted-Script-Parameters
 function DecryptString() {
     # Usage: ~$ DecryptString "Encrypted String" "Salt" "Passphrase"
     echo "${1}" | /usr/bin/openssl enc -aes256 -d -a -A -S "${2}" -k "${3}"
 }
 
 # Decrypt password
-apiUser=$(DecryptString $4 '' '')
-apiPass=$(DecryptString $5 '' '')
-
-# API URL
-apiURL="https://your.jss.com:8443"
-
-# Adobe SWID file
-adobeFile="/Library/Application Support/regid.1986-12.com.adobe/regid.1986-12.com.adobe_V7{}CreativeCloudEnt-1.0-Mac-GM-MUL.swidtag"
+apiUser=$(DecryptString $4 '$saltgoeshere' '$passphrasegoeshere')
+apiPass=$(DecryptString $5 '$saltgoeshere' '$passphrasegoeshere')
 
 ################################
 # Get the username of the logged in user
-loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}' )
 
 # Make sure someone is logged in
 if [[ -z "$loggedInUser" ]]; then
@@ -33,32 +64,50 @@ if [[ -z "$loggedInUser" ]]; then
     exit 1
 fi
 
+## Check Acrobat Pro
+if [[ ! -e "$acrobatTool" ]]; then
+    jamf policy -event $DownloadAcrobatTool
+fi
+
+SerialCheck=$("$acrobatTool" 2> /dev/null)
+if [[ -e "/Applications/Adobe Acrobat DC"  ]] || [[ -e "/Applications/Adobe Acrobat 2015" ]] || [[ -e "/Applications/Adobe Acrobat XI Pro" ]] || [[ -e "/Applications/Adobe Acrobat X Pro" ]] || [[ -e "/Applications/Adobe Acrobat 9 Pro" ]] || [[ -e "/Applications/Adobe Acrobat 8 Professional" ]]; then
+    if [[ "$SerialCheck" =~ "No expiring/expired serial number" ]]; then
+        resultACR="$AcrobatNamedUserInstall"
+    elif [[ "$SerialCheck" =~ "EncryptedSerial" ]]; then
+        resultACR="$AcrobatSerialInstall"
+    else 
+        resultACR="$AcrobatError"
+    fi
+else
+    resultACR="$AcrobatNotInstalled"
+fi
+
 ## See if file is actually present, if it is, result is echod. Else, result shows TYPE:loggedInUser
-if [[ ! -f "$adobeFile" ]]; then
-    result="Creative Cloud Not Installed"
+if [[ ! -f "$adobeCCFile" ]]; then
+    resultCC="$CCNotInstalled"
 else
     ## Find channel type
     # VOLUME == Full installer      
     # SUBSCRIPTION == named-user        
     # UNKNOWN == uninstalled or other error
-    channel_type=$(xmllint --xpath "//*[local-name()='license_linkage']/*[local-name()='channel_type']" "$adobeFile" \
+    channel_type=$(xmllint --xpath "//*[local-name()='license_linkage']/*[local-name()='channel_type']" "$adobeCCFile" \
         | sed -e 's/<swid:channel_type>//;s/<\/swid:channel_type>//')
     if [[ "$channel_type" =~ "VOLUME" || "$channel_type" =~ "SUBSCRIPTION" ]]; then
-        result=$channel_type:$loggedInUser
-        echo $result
+        resultCC=$channel_type:$loggedInUser
     elif [[ "$channel_type" =~ "UNKNOWN" ]]; then
-        result="Creative Cloud uninstalled"
-        echo $result
+        resultCC="$CCUninstalled"
     else
-        result="Creative Cloud error"
-        echo $result
+        resultCC="$CCError"
     fi
 fi  
+
+echo "Acrobat: $resultACR"
+echo "CC: $resultCC"
 
 ################################
 ## API upload values
 udid=$(/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/awk '/Hardware UUID:/ { print $3 }')
-xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\"?><computer><extension_attributes><extension_attribute><name>AdobeCCLicense:Username</name><value>$result</value></extension_attribute></extension_attributes></computer>"
+xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\"?><computer><extension_attributes><extension_attribute><id>$CCLicenseEA</id><value>$resultCC</value></extension_attribute><extension_attribute><id>$AcrobatLicenseEA</id><value>$resultACR</value></extension_attribute></extension_attributes></computer>"
 
 # Update the Adobe CC Extention Attribute
 UpdateAPI (){
