3ec8d669d2b3a266c82b3fe147f70d324444c35d
James <james.journey@gmail.com>
2020-10-22 14:31:27 -0700

    deleting

--- jamf/EAs/macOS_SIPstatus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-OSMajorVersion=$(sw_vers -productVersion | cut -d '.' -f 1,2)
-
-if [[ "$OSMajorVersion" == "10.11" || "$OSMajorVersion" == "10.12" ]] ; then
-    echo "<result>`csrutil status | awk '{ print $5; exit }' | sed 's/.$//'`</result>"
-else
-    echo "<result>SIP not applicable</result>"
-fi
