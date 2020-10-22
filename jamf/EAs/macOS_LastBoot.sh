86e186a954a6acc7aabe6850a8106a1bb3248751
James <james.journey@gmail.com>
2020-10-22 14:31:37 -0700

    deleting

--- jamf/EAs/macOS_LastBoot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-lastBootRaw=$(sysctl kern.boottime | awk -F'[= |,]' '{print $6}')
-lastBootFormat=$(date -jf "%s" "$lastBootRaw" +"%Y-%m-%d %T")
-
-echo "<result>$lastBootFormat</result>"
